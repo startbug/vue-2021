b3e86e811fd7ac92ba94c415bc4f7f5d88042146
startbug <51115577+startbug@users.noreply.github.com>
2021-08-05 00:58:18 +0800

    添加默认导出案例

---
+++ day1/code/01.默认到处.js
@@ -0,0 +1,11 @@
+let n1 = 10;
+let n2 = 15;
+
+function show() {
+    console.log("show aaaaa");
+}
+
+export default {
+    n1,
+    show
+}
---
+++ day1/code/02.默认导入.js
@@ -0,0 +1,3 @@
+import test from "./01.默认到处.js"
+
+console.log(test);
